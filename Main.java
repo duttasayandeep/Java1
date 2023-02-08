fcf109b564936e8ff11bfece627b0e585f23e40b
duttasayandeep <sayandeepdutta007@gmail.com>
2023-02-09 00:00:55 +0530

    firstcommit

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int num1 =34;
+        double num2;
+        num2 = num1;
+        System.out.println(num2);
+
+        double num3 = 23.8;
+        int num4;
+        num4=(int)num3;
+        System.out.println(num4);
+
+    }
+}
